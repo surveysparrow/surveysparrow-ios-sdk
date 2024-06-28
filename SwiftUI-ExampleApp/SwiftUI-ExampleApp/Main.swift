e967ab2019d6312230733f4a563726d2d2edb2b3
Gokulkrishna-Raju <gokulkrishna.raju@surveysparrow.com>
2024-06-28 13:47:01 +0530

    Form Feild

--- SwiftUI-ExampleApp/SwiftUI-ExampleApp/Main.swift
+++ SwiftUI-ExampleApp/SwiftUI-ExampleApp/Main.swift
@@ -10,6 +10,7 @@ import SurveySparrowSdk
 
  
 @main
+@available(iOS 15.0, *)
 struct ExampleApp: App {
     var body: some Scene {
         WindowGroup {
@@ -17,20 +18,48 @@ struct ExampleApp: App {
         }
     }
 }
- 
+
+@available(iOS 15.0, *)
 struct ContentView: View {
     
     @State private var isModalPresented : Bool = false
     @State private var showEmbedSurvey : Bool = false
     @State private var isValidationPresented : Bool = false
     
-    var domain: String = "<account-domain>"
-    var token: String = "<sdk-token>"
-    var sparrowLang: String = "<your-preferred-language-code>"
+    @State private var domain: String = "<account-domain>"
+    @State private var token: String = "<sdk-token>"
+    @State private var sparrowLang: String = "<your-preferred-language-code>"
     var params: [String:String] = ["emailaddress": "email@email.com", "email": "email@email.com"]
     
     var body: some View {
         VStack {
+            TextField(
+                "Domain",
+                text: $domain
+            )
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+            .padding()
+            
+            TextField(
+                "Token",
+                text: $token
+            )
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+            .padding()
+            
+            TextField(
+                "$parrowLang",
+                text: $sparrowLang
+            )
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+            .padding()
+            
             Button{
                 isModalPresented = true
             } label:{
