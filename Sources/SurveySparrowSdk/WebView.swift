9b93d2cc8669c67ff1037b4b45dbed05bcc78536
Gokulkrishna-Raju <gokulkrishna.raju@surveysparrrow.com>
2024-12-27 11:48:55 +0530

    Reverting Survey loaded callback change

--- Sources/SurveySparrowSdk/WebView.swift
+++ Sources/SurveySparrowSdk/WebView.swift
@@ -64,6 +64,7 @@ struct WebViewRepresentable: UIViewRepresentable {
 
     class Coordinator: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
         
+        private var surveyLoaded: String = "surveyLoadStarted"
         private var surveyCompleted: String = "surveyCompleted"
         private var spotCheckData: String = "spotCheckData"
         private var closeModel: String = "closeModal"
@@ -72,17 +73,25 @@ struct WebViewRepresentable: UIViewRepresentable {
             if self.parent.delegate != nil {
                 let response = message.body as! [String: AnyObject]
                 let responseType = response["type"] as! String
-                if responseType == surveyCompleted {
-                    self.parent.state.end()
-                    self.parent.delegate.handleSurveyResponse(response: response)
+                if responseType == surveyLoaded {
+                    if self.parent.delegate != nil {
+                        self.parent.delegate.handleSurveyLoaded(response: response)
+                    }
+                } else if responseType == surveyCompleted {
+                    if self.parent.delegate != nil {
+                        self.parent.state.end()
+                        self.parent.delegate.handleSurveyResponse(response: response)
+                    }
                 } else if responseType == spotCheckData {
-                    self.parent.delegate.handleSurveyLoaded(response: [:])
-                    if let currentQuestionSize = response["data"]?["currentQuestionSize"] as? [String: Any],
-                       let height = currentQuestionSize["height"] as? Double {
-                        self.parent.state.currentQuestionHeight = height
+                    if self.parent.delegate != nil {
+                        if let currentQuestionSize = response["data"]?["currentQuestionSize"] as? [String: Any],
+                           let height = currentQuestionSize["height"] as? Double {
+                            self.parent.state.currentQuestionHeight = height
+                        }
                     }
                 }
             }
+
         }
         
         var parent: WebViewRepresentable
