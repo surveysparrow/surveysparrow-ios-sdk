4ded1bec1fe760a7b6fab48e1ed55808f8433ab1
K Kalaiselvan <kalaiselvan.k@surveysparrow.com>
2025-07-14 13:27:13 +0530

    fix: concurrency version fix

--- Sources/SurveySparrowSdk/SsSpotcheckDelegate.swift
+++ Sources/SurveySparrowSdk/SsSpotcheckDelegate.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+@available(iOS 13.0, *)
 public protocol SsSpotcheckDelegate {
     func handleSurveyResponse(response: [String: AnyObject]) async
     func handleSurveyLoaded(response: [String: AnyObject]) async
@@ -7,6 +8,7 @@ public protocol SsSpotcheckDelegate {
     func handlePartialSubmission(response: [String: AnyObject]) async
 }
 
+@available(iOS 13.0, *)
 public extension SsSpotcheckDelegate {
     func handleSurveyResponse(response: [String: AnyObject]) async {}
     func handleSurveyLoaded(response: [String: AnyObject]) async {}
